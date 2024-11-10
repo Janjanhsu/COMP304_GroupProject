d92ad25286cdc320a6bc71383601c7afd04ba886
YI-CHEN HSU <126523675+Janjanhsu@users.noreply.github.com>
2024-11-09 23:14:06 -0500

    Reduce errors

--- yichen_kwokwing_COMP304Sec001_Lab03/app/src/main/java/com/example/yichen/yichen_kwokwing_comp304sec001_lab03/data/remote/WeatherApi.kt
+++ yichen_kwokwing_COMP304Sec001_Lab03/app/src/main/java/com/example/yichen/yichen_kwokwing_comp304sec001_lab03/data/remote/WeatherApi.kt
@@ -1,5 +1,7 @@
 package com.example.yichen.yichen_kwokwing_comp304sec001_lab03.data.remote
 
+
+import com.example.yichen.yichen_kwokwing_comp304sec001_lab03.model.Weather
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
