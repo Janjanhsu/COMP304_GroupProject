be97bba077f204f4cb06e2b978ad82006627297b
winghk00 <winghk@gmail.com>
2024-11-19 13:00:12 -0500

    updated GUI

--- yichen_kwokwing_COMP304Sec001_Lab04/app/src/main/java/com/example/yichen/yichen_kwokwing_comp304sec001_lab04/util/commonUtil.kt
+++ yichen_kwokwing_COMP304Sec001_Lab04/app/src/main/java/com/example/yichen/yichen_kwokwing_comp304sec001_lab04/util/commonUtil.kt
@@ -1,11 +1,14 @@
 package com.example.yichen.yichen_kwokwing_comp304sec001_lab04.util
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import androidx.navigation.NavController
 import com.example.yichen.yichen_kwokwing_comp304sec001_lab04.R
 import com.example.yichen.yichen_kwokwing_comp304sec001_lab04.data.Locations
 import com.example.yichen.yichen_kwokwing_comp304sec001_lab04.data.readCsv
 import java.io.InputStream
+
 
 fun getLocations (navController: NavController): Locations {
     val context: Context = navController.context
@@ -13,4 +16,14 @@ fun getLocations (navController: NavController): Locations {
 
     val locations = readCsv(inputStream)
     return locations
+}
+
+fun getDrawableIDByName(name:String, navController: NavController): Int {
+    val context: Context = navController.context
+    val resources: Resources = context.getResources()
+    val resourceId = resources.getIdentifier(
+        name, "drawable",
+        context.getPackageName()
+    )
+    return resourceId
 }
--- yichen_kwokwing_COMP304Sec001_Lab04/app/src/main/java/com/example/yichen/yichen_kwokwing_comp304sec001_lab04/view/LocationListScreen.kt
+++ yichen_kwokwing_COMP304Sec001_Lab04/app/src/main/java/com/example/yichen/yichen_kwokwing_comp304sec001_lab04/view/LocationListScreen.kt
@@ -1,17 +1,23 @@
 package com.example.yichen.yichen_kwokwing_comp304sec001_lab04.view
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,12 +28,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.yichen.yichen_kwokwing_comp304sec001_lab04.R
 import com.example.yichen.yichen_kwokwing_comp304sec001_lab04.data.Location
 import com.example.yichen.yichen_kwokwing_comp304sec001_lab04.navigation.Screen
+import com.example.yichen.yichen_kwokwing_comp304sec001_lab04.util.getDrawableIDByName
 import com.example.yichen.yichen_kwokwing_comp304sec001_lab04.util.getLocations
 
 @Composable
@@ -45,12 +53,15 @@ fun LocationListScreen(category: String, navController: NavController) {
 
 @Composable
 fun LocationCard(location: Location, navController: NavController, onClick: () -> Unit) {
-    Card(
+    Card(shape = RoundedCornerShape(90.dp),
         modifier = Modifier
             .padding(16.dp)
             .clickable(onClick = onClick)
     ) {
-        Row(modifier = Modifier.padding(10.dp)) {
+        Column(modifier = Modifier.padding(10.dp)
+            .height(300.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Start,
@@ -58,19 +69,29 @@ fun LocationCard(location: Location, navController: NavController, onClick: () -
             ) {
                 Box(
                     modifier = Modifier
-                        .size(120.dp)
+                        //.size(120.dp)
                         .clip(CircleShape)
-                        .background(Color.Red)
+                        //.background(Color.Red)
                 ){
+                    val filename = location.photo.substringBeforeLast(".")
+                    Log.i("myApp", filename)
+                    val id = getDrawableIDByName(filename, navController)
                     Image(
-                        painter = painterResource(id = R.drawable.star),
+                        painter = painterResource(id = id),
                         contentDescription = null,
-                        contentScale = ContentScale.Crop
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
                     )
-                }
-                Text(" ${location.name}", fontSize = 30.sp, fontWeight = FontWeight.Bold)
-            }
-        }
 
+                }
+
+            }
+
+        }
+        Column (modifier = Modifier
+            .padding(18.dp)){
+            Text(" ${location.name}", fontSize = 30.sp, fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
+        }
     }
 }
